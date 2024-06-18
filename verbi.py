1aeb0b6524062d7aa47784b08b1a9eb70108b1df
AntonioSpallone1 <andersenspallone54@gmail.com>
2024-06-18 12:44:53 +0200

    Update verbi.py

--- verbi.py
+++ verbi.py
@@ -2,10 +2,11 @@ import requests
 
 
 def main():
-
+    # Input dell'utente per l'host e la porta
     host = input("Inserisci url dell'host: ")
     porta = input("Inserisci porta: ")
-
+    
+    # Costruzione dell'URL in base alla porta
     if porta == "80":
         url = f"http://{host}:{porta}"
     elif porta == "443":
@@ -14,12 +15,15 @@ def main():
         print("Controlla la porta inserita!")
         return
 
-
+    # Stampa dell'URL verificato
     print(f"Verifico: {url}")
 
     try:
+        # Invio della richiesta HTTP OPTIONS
         risposta = requests.options(url)
+        # Controllo del codice di stato della risposta
         if risposta.status_code == 200:
+             # Controllo se l'intestazione 'Allow' è presente nella risposta
             if 'Allow' in risposta.headers:
                 metodi = risposta.headers['Allow']
                 print(f"Ecco i metodi abilitati: {metodi}")
@@ -28,6 +32,7 @@ def main():
         else:
             print(f"Codice di stato: {risposta.status_code}")
     except requests.RequestException as e:
+         # Gestione delle eccezioni per errori nella richiesta
         print(f"Errore nella richiesta: {e}")
 
 
